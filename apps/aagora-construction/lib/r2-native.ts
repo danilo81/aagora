71c01db01f6ba9ff796da04274bb62673b4ed0aa
Yogurt <danilopatriota81@gmail.com>
2026-05-19 09:56:33 -0400

    2

--- apps/aagora-construction/lib/r2-native.ts
+++ apps/aagora-construction/lib/r2-native.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { getCloudflareContext } from '@opennextjs/cloudflare';
 
 interface R2Bucket {
